881b50461954df9ae518db0133941307e5cef732
JuzChoco <97875095+JuzChoco@users.noreply.github.com>
2022-01-26 00:28:32 +0800

    Add files via upload

---
+++ Wk1Q2a.java
@@ -0,0 +1,6 @@
+public class Wk1Q2a {
+    
+    public static void main(String[] args){
+        System.out.println("Hello, I am Oliver");
+    }
+}
---
+++ Wk1Q2c.java
@@ -0,0 +1,13 @@
+public class Wk1Q2c {
+
+    public static void main(String[] args){
+
+        for (int x = 102; x >= 66; x--){
+
+            if (x % 2 != 0){
+            System.out.println("Value of x: " + x);
+            
+            }
+        }
+    }
+}
